8327b060baa4e243c7f3336f37bec856c31da063
nicholasxwang <80929436+nicholasxwang@users.noreply.github.com>
2022-01-10 12:19:36 -0800

    added copied code :)

--- main.ino
+++ main.ino
@@ -1,57 +1,165 @@
-/*
-  LED bar graph
+#include <Servo.h>
 
-  Turns on a series of LEDs based on the value of an analog sensor.
-  This is a simple way to make a bar graph display. Though this graph uses 10
-  LEDs, you can use any number by changing the LED count and the pins in the
-  array.
+Servo myservo;   // create 4 servo objects to control the servos
+Servo myservo1;
+Servo myservo2;
+Servo myservo3;
 
-  This method can be used to control any series of digital outputs that depends
-  on an analog input.
+int angle = 90; // initial angle  for servo
+int angle1 = 90;
+int angle2 = 90;
+int angle3 = 90;
+int angleStep = 5;
 
-  The circuit:
-  - LEDs from pins 2 through 11 to ground
-
-  created 4 Sep 2010
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/BarGraph
-*/
-
-// these constants won't change:
-const int analogPin = A0;   // the pin that the potentiometer is attached to
-const int ledCount = 10;    // the number of LEDs in the bar graph
-
-int ledPins[] = {
-        2, 3, 4, 5, 6, 7, 8, 9, 10, 11
-};   // an array of pin numbers to which LEDs are attached
-
+#define LEFT 10   // pin 10 is connected to left button
+#define RIGHT  11  // pin 11 is connected to right button
+#define LEFT1 2 // pin 2 is connected to right button
+#define RIGHT1 3 // pin 3 is connected to right button
+#define LEFT2 6 // pin 6 is connected to right button
+#define RIGHT2 7 // pin 7 is connected to right button
+#define RIGHT3 4 // pin 4 is connected to right button
+#define LEFT3 5 // pin 5 is connected to right button
 
 void setup() {
-    // loop over the pin array and set them all to output:
-    for (int thisLed = 0; thisLed < ledCount; thisLed++) {
-        pinMode(ledPins[thisLed], OUTPUT);
-    }
+ Serial.begin(9600);  //  setup serial
+ myservo.attach(8);  // attaches the servo on pin 9 to the servo object
+ myservo1.attach(9);
+ myservo2.attach(12);
+ myservo3.attach(13);
+ pinMode(LEFT, INPUT_PULLUP); // assign pins as input left button
+pinMode(RIGHT, INPUT_PULLUP); // assing pins as input for right button
+pinMode(LEFT1, INPUT_PULLUP);
+pinMode(RIGHT1, INPUT_PULLUP);
+pinMode(LEFT2, INPUT_PULLUP);
+pinMode(RIGHT2, INPUT_PULLUP);
+pinMode(LEFT3, INPUT_PULLUP);
+pinMode(RIGHT3, INPUT_PULLUP);
+ 
+myservo.write(angle); // send servo to the middle at 90 degrees
+myservo1.write(angle1); // send servo to the middle at 90 degrees
+myservo1.write(angle2); // send servo to the middle at 90 degrees
+myservo1.write(angle3); // send servo to the middle at 90 degrees
+    
 }
 
 void loop() {
-    // read the potentiometer:
-    int sensorReading = analogRead(analogPin);
-    // map the result to a range from 0 to the number of LEDs:
-    int ledLevel = map(sensorReading, 0, 1023, 0, ledCount);
+////////////////////////////////////////////////////////////////////////
+ while(digitalRead(RIGHT) == LOW){
 
-    // loop over the LED array:
-    for (int thisLed = 0; thisLed < ledCount; thisLed++) {
-        // if the array element's index is less than ledLevel,
-        // turn the pin for this element on:
-        if (thisLed < ledLevel) {
-            digitalWrite(ledPins[thisLed], HIGH);
-        }
-            // turn off all pins higher than the ledLevel:
-        else {
-            digitalWrite(ledPins[thisLed], LOW);
-        }
-    }
+   if (angle > 0 && angle <= 180) {
+     angle = angle - angleStep;
+      if(angle < 0){
+       angle = 0;
+      }
+else{
+     myservo.write(angle); // move the servo to desired angle
+      }
+   }
+   
+ delay(50); // waits for the servo to get there
+ }
+////////////////////////////////////////////////////////////////////////
+while(digitalRead(LEFT) == LOW){
+   if (angle >= 0 && angle <= 180) {
+     angle = angle + angleStep;
+     if(angle >180){
+       angle =180;
+      }
+ else{
+     myservo.write(angle); // move the servo to desired angle
+      }
+   }
+   
+ delay(50); // waits for the servo to get there
+ }
+////////////////////////////////////////////////////////////////////////
+ while(digitalRead(RIGHT1) == LOW){
+
+   if (angle1 > 0 && angle1 <= 180) {
+     angle1 = angle1 - angleStep;
+      if(angle1 < 0){
+       angle1 = 0;
+      }else{
+     myservo1.write(angle1); // move the servo to desired angle
+      }
+   }
+   
+ delay(50); // waits for the servo to get there
+ }
+////////////////////////////////////////////////////////////////////////
+ while(digitalRead(LEFT1) == LOW){
+
+   if (angle1 >= 0 && angle1 <= 180) {
+     angle1 = angle1 + angleStep;
+     if(angle1 >180){
+       angle =180;
+      }else{
+     myservo1.write(angle1); // move the servo to desired angle
+      }
+   }
+   
+ delay(50); // waits for the servo to get there
+ }
+////////////////////////////////////////////////////////////////////////
+ while(digitalRead(RIGHT2) == LOW){
+
+   if (angle2 > 0 && angle2 <= 180) {
+     angle2 = angle2 - angleStep;
+      if(angle2 < 0){
+       angle2 = 0;
+      }
+ else{
+     myservo2.write(angle2); // move the servo to desired angle
+      }
+   }
+   
+ delay(50); // waits for the servo to get there
+ }
+////////////////////////////////////////////////////////////////////////
+ while(digitalRead(LEFT2) == LOW){
+
+   if (angle2 >= 0 && angle2 <= 180) {
+     angle2 = angle2 + angleStep;
+     if(angle2 >180){
+       angle =180;
+      }
+ else{
+     myservo2.write(angle2); // move the servo to desired angle
+      }
+   }
+   
+ delay(50); // waits for the servo to get there
+ }
+////////////////////////////////////////////////////////////////////////
+ while(digitalRead(RIGHT3) == LOW){
+
+   if (angle3 > 0 && angle3 <= 180) {
+     angle3 = angle3 - angleStep;
+      if(angle3 < 0){
+       angle3 = 0;
+      }
+ else{
+     myservo3.write(angle3); // move the servo to desired angle
+      }
+   }
+   
+ delay(50); // waits for the servo to get there
+ }
+////////////////////////////////////////////////////////////////////////
+ while(digitalRead(LEFT3) == LOW){
+
+   if (angle3 >= 0 && angle3 <= 180) {
+     angle3 = angle3 + angleStep;
+     if(angle3 >180){
+       angle =180;
+      }
+ else{
+     myservo3.write(angle3); // move the servo to desired angle
+      }
+   }
+   
+ delay(50); // waits for the servo to get there
+ }
 }
+//http://www.makosite.one/robotic-arm.php
+//Copied Code
